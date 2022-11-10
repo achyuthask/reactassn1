d7c4f8ceecfe29d54751d812ccb14b00592136a9
achyuthask <96536973+achyuthask@users.noreply.github.com>
2022-11-10 23:48:47 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class = "text">
+    <h1>ABOUT US</h1>
+
+    <h4>Zappos.com is an American online shoe and clothing retailer based in Las Vegas, Nevada, United States.
+      The company was founded in 1999 by Nick Swinmurn and launched under the domain name Shoesite.com.
+       In July 2009, Amazon acquired Zappos in an all-stock deal worth around $1.2 billion at the time.
+        Amazon purchased all of the outstanding shares and warrants from Zappos for 10 million shares of Amazon's 
+        common stock and provided $40 million in cash and restricted stock for the Zappos employees.</h4>
+
+        <h4>Zappos was founded in 1999 by Nick Swinmurn.[5] Swinmurn launched the company with Tony Hsieh and Alfred Lin,
+           who invested $2 million through their investment firm Venture Frogs.
+            The company was officially launched online in 1999 as "ShoeSite.com".
+
+In July 1999, the company's name was changed from ShoeSite to Zappos after "zapatos",
+ the Spanish word meaning "shoes".In 2000, Venture Frogs invested in the business and Zappos moved into their office space.
+ In 2001, Hsieh came on board as co-CEO with Nick Swinmurn</h4>
+   </div>
   );
 }
 
